aee6bdd9cf501059f245cd7bc247a41becc5bd30
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2023-07-03 21:44:14 +0530

    app color changed

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -1,21 +1,73 @@
-import React, { useState } from 'react'
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+import { useToast } from "@chakra-ui/react";
 
 const Signup = () => {
-    
-    const [show, setShow] = useState(false);
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
-    const [confirmpassword, setConfirmpassword] = useState()
-    const [password, setPassword] = useState()
-    const [pic, setPic] = useState()
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [confirmpassword, setConfirmpassword] = useState();
+  const [password, setPassword] = useState();
+  const [pic, setPic] = useState();
+  const [picLoading, setPicLoading] = useState(false);
+  const toast = useToast();
 
+  const handleClick = () => setShow(!show);
 
-    const handleClick = () => setShow(!show);
+  const postDetails = (pics) => {
+    setPicLoading(true);
+    if (pics === undefined) {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
 
-    const postDetails = (pics) => { };
-    
-    const submitHandler = () => { };
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "chat-app");
+      data.append("cloud_name", "dywfdcbje");
+      fetch("https://api.cloudinary.com/v1_1/dywfdcbje", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPic(data.url.toString());
+          console.log(data.url.toString());
+          setPicLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setPicLoading(false);
+        });
+    } else {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPicLoading(false);
+      return;
+    }
+  };
+
+  const submitHandler = () => {};
 
   return (
     <VStack spacing="5px" color="black">
@@ -65,28 +117,29 @@ const Signup = () => {
             </Button>
           </InputRightElement>
         </InputGroup>
-          </FormControl>
-          
-          <FormControl id='pic'>
-              <FormLabel>Upload your Picture</FormLabel>
-              <Input
-                  type='file'
-                  p={1.5}
-                  accept='image/*'
-                  onChange={(e) => postDetails(e.target.files[0])}
-              />
-              
-          </FormControl>
+      </FormControl>
 
-          <Button colorScheme='blue'
-              width='100%'
-              style={{ marginTop: 15 }}
-              onClick={submitHandler}
-          >
-             Sign Up 
-          </Button>
+      <FormControl id="pic">
+        <FormLabel>Upload your Picture</FormLabel>
+        <Input
+          type="file"
+          p={1.5}
+          accept="image/*"
+          onChange={(e) => postDetails(e.target.files[0])}
+        />
+      </FormControl>
+
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+        isLoading={picLoading}
+      >
+        Sign Up
+      </Button>
     </VStack>
   );
-}
+};
 
-export default Signup
+export default Signup;
