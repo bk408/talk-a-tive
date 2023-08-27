3bb09f988a11a11e3d037b085c408885ec6089f2
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2023-08-27 10:17:35 +0530

    signup page added in app

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -54,6 +54,7 @@ const HomePage = () => {
             </TabPanel>
             <TabPanel>
               <Signup />
+              <Signup />
             </TabPanel>
           </TabPanels>
         </Tabs>
