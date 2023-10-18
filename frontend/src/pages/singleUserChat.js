2035ffa2fd254a4b0034932731fcd34793e87b90
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2023-10-18 20:36:57 +0530

    css updated for single user chat

--- frontend/src/pages/singleUserChat.js
+++ frontend/src/pages/singleUserChat.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const singleUserChat = () => {
   return (
-    <div>
+    <div className="container">
       <h1>singleUserChat</h1>
       <p>This is single user chat</p>
     </div>
